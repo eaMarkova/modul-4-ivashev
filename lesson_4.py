86bad6e2720fe7f5839f85db06fbcc2f220628bf
eaMarkova <akularin11@gmail.com>
2025-03-21 20:44:36 +0300

    2 вариант

--- lesson_4.py
+++ lesson_4.py
@@ -45,9 +45,6 @@ my_stack.pop()
 
 # Проверяем, пуст ли стек
 print("Стек пуст?", my_stack.is_empty())
-my_stack.pop()
-my_stack.pop()
-my_stack.pop()
 
 # Выводим размер стека
 print("Размер стека:", my_stack.size())
