4552c1b343e99482e70c96a68e0aa871e3c31c03
eaMarkova <akularin11@gmail.com>
2025-03-21 18:36:39 +0300

    Фиксация 1 варианта решения

--- lesson_5.py
+++ lesson_5.py
@@ -1,5 +1,5 @@
-
-
+'''''''''
+# 1 вариант решения
 skobki = input()
 stack = []
 is_good = True
@@ -25,5 +25,40 @@ if is_good and len(stack)==0:
     print('YES')
 else:
     print('NO')
+'''''''''''
+
+class Stack:
+    # список для хранения элементов стека.
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        #  который будет проверять, пуст ли стек.
+        return len(self.items) == 0
+
+    def push(self, item):
+        # который будет добавлять элемент в стек. Мы будем добавлять элемент в конец списка
+        self.items.append(item)
+
+    def pop(self):
+        #  который будет удалять и возвращать элемент из вершины стека. При этом мы проверим, не пуст ли стек перед попыткой извлечения элемента.
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            raise IndexError("Стек пуст")
+
+    def peek(self):
+        #  который будет возвращать элемент из вершины стека без его удаления. Также проверим, не пуст ли стек перед попыткой просмотра элемента.
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            raise IndexError("Стек пуст")
+
+    def size(self):
+        #который будет возвращать количество элементов в стеке.
+        return len(self.items)
+    
+    # Создаем экземпляр стека
+my_stack = Stack()
 
 
