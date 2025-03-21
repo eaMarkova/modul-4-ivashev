748aa63ae3297f0179ecca8dfc910b70be562e87
eaMarkova <akularin11@gmail.com>
2025-03-21 18:31:23 +0300

    Урок 5

---
+++ lesson_5.py
@@ -0,0 +1,29 @@
+
+
+skobki = input()
+stack = []
+is_good = True
+for i in skobki:
+     
+    if i in '({[':
+        stack.append(i)
+    elif i in "]})":
+        if not stack:
+            is_good = False
+            break
+        open_bracket = stack.pop()
+        if open_bracket == '(' and i == ')':
+            continue
+        if open_bracket == '{' and i == '}':
+            continue
+        if open_bracket == '[' and i == ']':
+            continue
+        is_good = False
+        break
+
+if is_good and len(stack)==0:
+    print('YES')
+else:
+    print('NO')
+
+
